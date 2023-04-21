746507c7633b659a9f3ed70eebd59c942788f4ad
Uriel Mata Castellanos <urielmata37@hotmail.com>
2023-04-20 22:50:21 -0600

    XD

---
+++ 2P-Leer-archivo/2P-Leer-archivo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2P_Leer_archivo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LeerArchivo a=new LeerArchivo();
+            a.cantidadRenglones();
+        }
+
+        class LeerArchivo
+        {
+            StreamReader archivo = null;
+            int contador = 0;
+            String renglon;
+
+            public LeerArchivo()
+            {
+                Console.WriteLine("---Texto---");
+                archivo = new StreamReader(@"C:\Uriel\Universidad\Segundo semestre\Segundo parcial\Programacion orientada a entornos visuales\Programas\2P-Leer-archivo\2P-Leer-archivo\TheBestSongEver.txt");
+                while ((renglon = archivo.ReadLine()) != null)
+                {
+                    Console.WriteLine(renglon);
+                    contador++;
+                }
+            }
+
+            public void cantidadRenglones()
+            {
+                Console.WriteLine("\nEl texto tiene {0} renglones", contador);
+            }
+
+            ~LeerArchivo()
+            {
+                archivo.Close();
+            }
+        }
+    }
+}

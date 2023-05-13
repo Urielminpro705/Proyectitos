2bcc29404c6fd48e0f4b230f7397c157d9acbf40
Urielminpro705 <urielmata37@hotmail.com>
2023-05-13 14:50:26 -0600

    XD

--- Gato/Gato/obj/Debug/Gato_Content.g.i.cs
+++ Gato/Gato/obj/Debug/Gato_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("post.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("titulo.jpg")]
 
 
